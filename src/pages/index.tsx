9c2f6480d7490b375fcddfc134df0c4d1aca751a
Raj-9747 <rajfachara9747@gmail.com>
2025-05-21 12:47:48 +0530

    Add login button with navigation to the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
+import Link from "next/link"; // Import Link for navigation
 
 export default function Home() {
   return (
     <>
       <div>
         <h1>Hello</h1>
+        <Link href="/login">
+          <button>Login</button>
+        </Link>
       </div>
     </>
   );
