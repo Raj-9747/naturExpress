b0e632dc3aaa120a70d619c9ae883f420f6cb1d4
Raj-9747 <rajfachara9747@gmail.com>
2025-05-21 17:50:40 +0530

    Add navigation link to home page in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
-import Link from "next/link"; // Import Link for navigation
+import Link from "next/link"; 
 
 export default function Home() {
   return (
@@ -10,6 +10,9 @@ export default function Home() {
         <h1>Hello</h1>
         <Link href="/login">
           <button>Login</button>
+        </Link>
+        <Link href="/home">
+          <button>Home</button>
         </Link>
       </div>
     </>
